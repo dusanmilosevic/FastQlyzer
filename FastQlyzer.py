0f995b6a8c104e59f730d6a592b9724ca99df21f
dusanmilosevic <milosevic.dusan@ymail.com>
2017-03-03 01:08:04 +0100

    Update FastQlyzer.py

--- FastQlyzer.py
+++ FastQlyzer.py
@@ -20,11 +20,11 @@ docs.append(filename2)
 
 file=open(docs[0],"r")
 line=file.readline()
-poorQualityBorder='*'
+poorQualityBorder=20
 
 if arguments['--qt'] == None:
-    poorQualityBorder = 9
-elif arguments['--qt']>=0 and arguments['--qt']<=93:
+    poorQualityBorder = 28
+elif int(arguments['--qt'])>=0 and int(arguments['--qt'])<=93:
     poorQualityBorder = int(arguments['--qt'])
 
 poorQualityBorderValue=poorQualityBorder
@@ -261,31 +261,35 @@ x=[i for i in range(numOfBasesPerRead)]
 fig0 = plt.figure(0)
 
 plt.title('Basic Statistics')
+
 plt.axis([0, 15, 0, 15])
 axes=plt.gca()
 axes.xaxis.set_major_formatter(plt.NullFormatter())
 axes.yaxis.set_major_formatter(plt.NullFormatter())
-plt.text(1, 13, 'Filename1:', ha='left', wrap=True,color='blue')
-plt.text(5, 13, filename1, ha='left', wrap=True)
-plt.text(1, 11, 'Filename2:', ha='left', wrap=True,color='blue')
-plt.text(5, 11, filename2, ha='left', wrap=True)
-plt.text(1, 9, 'Poor Quality Border:', ha='left', wrap=True,color='blue')
+plt.text(1, 13, 'Filename1:', ha='left', wrap=True,color='mediumslateblue')
+plt.text(4, 13, filename1, ha='left', wrap=True)
+plt.text(1, 11, 'Filename2:', ha='left', wrap=True,color='mediumslateblue')
+plt.text(4, 11, filename2, ha='left', wrap=True)
+plt.text(1, 9, 'Poor Quality Border:', ha='left', wrap=True,color='mediumslateblue')
 plt.text(7, 9, str(poorQualityBorder), ha='left', wrap=True)
-plt.text(1, 7, 'Total Sequences', ha='left', wrap=True,color='blue')
+plt.text(1, 7, 'Total Sequences', ha='left', wrap=True,color='mediumslateblue')
 plt.text(6, 7, str(numOfSequences), ha='left', wrap=True)
 g = "Seq. Flaged as"\
 	" poor Quality:"
-plt.text(1, 5, g, ha='left', wrap=True,color='blue')
+plt.text(1, 5, g, ha='left', wrap=True,color='mediumslateblue')
 plt.text(8, 5, str(numOfSeqflagedAsPoor), ha='left', wrap=True)
-plt.text(1, 3, 'Sequence Length:', ha='left', wrap=True, color='blue')
+plt.text(1, 3, 'Sequence Length:', ha='left', wrap=True, color='mediumslateblue')
 plt.text(6, 3, str(numOfBasesPerRead), ha='left', wrap=True)
-plt.text(1, 1, '%GC:', ha='left', wrap=True,color='blue')
+plt.text(1, 1, '%GC:', ha='left', wrap=True,color='mediumslateblue')
 plt.text(3, 1, str(round(numOfGCchar/float(totalChars)*100,1)), ha='left', wrap=True)
+plt.rcParams['axes.facecolor'] = 'gainsboro'
 #########################################################################################
 #########################################################################################
 
 #AVERAGE QUALIY PER POSITION
 fig1=plt.figure(1)
+#fig, ax = plt.subplots()
+#ax.fill(x,[9 for i in range(len(x))])
 axes=plt.gca()
 axes.set_ylim([0,60])
 axes.set_xlim([0,numOfBasesPerRead-2])
@@ -295,9 +299,15 @@ plt.title('Quality scores across all bases')
 plt.grid(True)
 blue_patch = mpatches.Patch(color='blue', label='Average(Mean) Quality')
 red_patch = mpatches.Patch(color='red', label='Median Quality')
-plt.legend(handles=[blue_patch,red_patch])
+greenyellow_patch=mpatches.Patch(color='greenyellow', label='Good Quality Zone (Theoretical)')
+lightsalmon_patch=mpatches.Patch(color='lightsalmon', label='Bad Quality Zone (Theoretical)')
+black_patch=mpatches.Patch(color='black', label='Default/Client-chosen Quality Border ')
+plt.legend(handles=[greenyellow_patch,lightsalmon_patch,blue_patch,red_patch, black_patch])
 #plt.xticks(arange(numOfBasesPerRead-2, step=5))
-plt.plot(x,sumOfQualRead,'b',x,medians,'r')
+bord=[poorQualityBorderValue for i in range(len(x))]
+plt.plot(x,sumOfQualRead,'b',x,medians,'r',x,bord,'black',x,[0 for i in range(len(medians))],'k|')
+plt.fill_between(x,0,[100 for i in range(len(x))],facecolor='greenyellow')
+plt.fill_between(x,0,[28 for i in range(len(x))],facecolor='lightsalmon')
 
 #########################################################################################
 #########################################################################################
@@ -305,13 +315,16 @@ plt.plot(x,sumOfQualRead,'b',x,medians,'r')
 #Per sequence quality score
 #Quality score distribution over all sequences
 fig2=plt.figure(2)
+x_ = np.arange(0,len(densityOfQualities),1)
+y_= [0 for i in range(len(x_))]
 axes=plt.gca()
 axes.set_xlim([0,50])
 plt.xlabel('Mean Sequence Quality(Phred Score)')
 #plt.ylabel('Proportion[%]')
 plt.title('Quality score distribution over all sequences')
 plt.grid(True)
-plt.plot(densityOfQualities,howMany,'r')
+plt.fill_between(densityOfQualities,0,howMany,facecolor='lightsalmon')
+
 
 #########################################################################################
 #########################################################################################
@@ -337,6 +350,7 @@ axes.set_xlim([0,numOfBasesPerRead-2])
 axes.set_ylim([0,100])
 
 plt.plot(x,numOfAperRead,'b',x,numOfTperRead,'g',x,numOfCperRead,'y',x,numOfGperRead,'r',lw=2)
+plt.plot(x,[0 for i in range(len(x))],'k|')
 
 ################################################################################################
 ################################################################################################
@@ -344,6 +358,7 @@ plt.plot(x,numOfAperRead,'b',x,numOfTperRead,'g',x,numOfCperRead,'y',x,numOfGper
 #Per base GC content
 #GC content across all bases
 fig4=plt.figure(4)
+plt.rcParams['axes.facecolor'] = 'linen'
 plt.xlabel('Position in read(bp)')
 plt.ylabel('Proportion[%]')
 plt.title('GC content across all bases')
@@ -360,21 +375,23 @@ plt.plot(x,GCcontentPerPositionInRead,'g')
 #Per Sequence GC Content
 #GC distribution over all sequences
 fig5=plt.figure(5)
+plt.rcParams['axes.facecolor'] = 'linen'
 plt.xlabel('Mean GC content (%)')
 #plt.ylabel('Proportion[%]')
 plt.title('GC distribution over all sequences')
 plt.grid(True)
 red_patch = mpatches.Patch(color='red', label='GC count per read')
 plt.legend(handles=[red_patch])
+
 plt.plot(GCdensity,GChowMany,'r')
 
 
 #Testing time!
-#plt.show()
+plt.show()
 
 #Saving graphs into a single pdf file
 import matplotlib.backends.backend_pdf
-pdf = matplotlib.backends.backend_pdf.PdfPages("report.pdf")
+pdf = matplotlib.backends.backend_pdf.PdfPages("FastQyzer_Report.pdf")
 
 pdf.savefig(fig0)
 pdf.savefig(fig1)
